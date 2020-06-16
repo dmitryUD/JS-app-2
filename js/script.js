c9776c3f35b4a66efe7e24913bf0a478ad60bad2
Dmitry Udovik <udovikdmitry@bk.ru>
2020-06-16 18:48:22 +0300

    16.06.2020

--- js/script.js
+++ js/script.js
@@ -17,75 +17,44 @@ P.S. Здесь есть несколько вариантов решения з
 
 'use strict';
 
-document.addEventListener('DOMContentLoaded', () => {
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против...",
+    ]
+};
 
-    const movieDB = {
-        movies: [
-            "Логан",
-            "Лига справедливости",
-            "Ла-ла лэнд",
-            "Одержимость",
-            "Скотт Пилигрим против..."
-        ]
-    };
+const adv = document.querySelectorAll('.promo__adv img'),
+    poster = document.querySelector('.promo__bg'),
+    genre = poster.querySelector('.promo__genre'),
+    movieList = document.querySelector('.promo__interactive-list'),
+    addForm = document.querySelector('form.add'),
+    addInput = addForm.querySelector('adding__input'),
+    checkbox = addForm.querySelector('input[type="checkbox"]'),
+    addButton = addForm.querySelector('button');
 
-    // задание всех переменных, необходимых для работы
-    const adv = document.querySelectorAll('.promo__adv img'),
-        poster = document.querySelector('.promo__bg'),
-        genre = poster.querySelector('.promo__genre'),
-        movieList = document.querySelector('.promo__interactive-list'), // список фильмов
-        addForm = document.querySelector('form.add'), // получение формы со страницы
-        addInput = addForm.querySelector('.adding__input'), // поле для ввода фильмов
-        checkbox = addForm.querySelector('[type="checkbox"]'); // галочка "Да"
-    
-    addForm.addEventListener('submit', (event) => {
-        event.preventDefault(); // отменить стандартное поведение браузера - не перезагружать страницу
+adv.forEach(item => {
+    item.remove();
+});
 
-        const newFilm = addInput.value; // содержится то, что ввел пользователь
-        const favorite = checkbox.checked;
+genre.textContent = "Драма";
 
-        if (newFilm) {
-            movieDB.movies.push(newFilm); // добавить фильм в конец массива
-            sortArr(movieDB.movies); // отсортировать массив
+poster.style.backgroundImage = 'url("img/bg.jpg")';
 
-            createMovieList(movieDB.movies, movieList);
-        }
-        event.target.reset();
-    });
+movieList.innerHTML = ''; // очищение элемента (блока)
+movieDB.movies.sort(); // сортировка по алфовиту
 
-    const deleteAdv = (arr) => {
-        adv.forEach(item => {
-            item.remove();
-        });
-    };
-    
-    const makeChanges = () => {
-        genre.textContent = "Драма";
-        poster.style.backgroundImage = 'url("img/bg.jpg")';
-    };
-
-    const sortArr = (arr) => {
-        arr.sort();
-    };
-
-    function createMovieList(films, parent) {
-        parent.innerHTML = '';
-
-        films.forEach((film, i) => { // film - каждый отдельный фильм, i - порядковый номер фильма
-            parent.innerHTML += `
+movieDB.movies.forEach((film, i) => { // film - каждый отдельный фильм, i - порядковый номер фильма
+    movieList.innerHTML += `
         <li class="promo__interactive-item">${i + 1} ${film}
             <div class="delete"></div>
         </li>
     `;
-        });
-    }
-
-    deleteAdv(adv);
-    makeChanges();
-    sortArr(movieDB.movies);
-    createMovieList(movieDB.movies, movieList);
-
 });
 
 
 
+// fdsddddddddddddddddddddddddddfsdfsd
