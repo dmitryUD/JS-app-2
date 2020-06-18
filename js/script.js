c65bffb6b8e01468793e6e14cb109bd762ea3032
Dmitry Udovik <udovikdmitry@bk.ru>
2020-06-18 14:52:10 +0300

    demo 18.06.2020

--- js/script.js
+++ js/script.js
@@ -17,73 +17,80 @@ P.S. Здесь есть несколько вариантов решения з
 
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против...",
-    ]
-};
+document.addEventListener('DOMContentLoaded', () => {
 
-const adv = document.querySelectorAll('.promo__adv img'),
-    poster = document.querySelector('.promo__bg'),
-    genre = poster.querySelector('.promo__genre'),
-    movieList = document.querySelector('.promo__interactive-list'),
-    addForm = document.querySelector('form.add'),
-    addInput = addForm.querySelector('.adding__input'),
-    checkbox = addForm.querySelector('input[type="checkbox"]');
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против...",
+        ]
+    };
+
+    const adv = document.querySelectorAll('.promo__adv img'),
+        poster = document.querySelector('.promo__bg'),
+        genre = poster.querySelector('.promo__genre'),
+        movieList = document.querySelector('.promo__interactive-list'),
+        addForm = document.querySelector('form.add'), // вся форма
+        addInput = addForm.querySelector('.adding__input'),
+        checkbox = addForm.querySelector('[type="checkbox"]');
     // addButton = addForm.querySelector("button");
 
-// ОСНОВА
-addForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const newFilm = addInput.value;
+    // ОСНОВА
+    addForm.addEventListener('submit', (event) => {
+        event.preventDefault(); // отмена стандартного поведения браузера
 
-    if (newFilm) {
-        if (newFilm.length > 21) {
-            let NewFilm = newFilm.substring(0, 21) + '...';
-            movieDB.movies.push(NewFilm);
-            addingFilm();
-            addForm.reset();
-        } else {
-            movieDB.movies.push(newFilm);
-            addingFilm();
-            addForm.reset();
+        const newFilm = addInput.value;
+        const favorite = checkbox.checked;
+
+        if (newFilm) {
+            if (newFilm.length > 21) {
+                let NewFilm = newFilm.substring(0, 21) + '...';
+                movieDB.movies.push(NewFilm);
+                createMovieList(movieDB.movies, movieList);
+                sortArr(movieDB.movies);
+                addForm.reset();
+            } else {
+                movieDB.movies.push(newFilm);
+                createMovieList(movieDB.movies, movieList);
+                addForm.reset();
+            }
         }
-    } 
-});
-
-function deleteAdv() {
-    adv.forEach(item => {
-        item.remove();
     });
-}
 
-function editGenre() {
-    genre.textContent = "Драма";
-}
+    const deleteAdv = (arr) => {
+        arr.forEach(item => {
+            item.remove();
+        });
+    };
 
-function editPoster() {
-    poster.style.backgroundImage = 'url("img/bg.jpg")';
-}
+    const makeChanges = () => {
+        genre.textContent = "Драма";
+        poster.style.backgroundImage = 'url("img/bg.jpg")';
+    };
 
-function addingFilm() {
-    movieList.innerHTML = ''; // очищение элемента (блока)
-    movieDB.movies.sort();
-    movieDB.movies.forEach((film, i) => { // film - каждый отдельный фильм, i - порядковый номер фильма
-        movieList.innerHTML += `
+    const sortArr = (arr) => {
+        arr.sort();
+    };
+
+    function createMovieList(films, parent) {
+        parent.innerHTML = '';
+        films.forEach((film, i) => { // film - каждый отдельный фильм, i - порядковый номер фильма
+            parent.innerHTML += `
         <li class="promo__interactive-item">${i + 1} ${film}
             <div class="delete"></div>
         </li>
     `;
-    });
-}
+        });
+    }
+
+    createMovieList(movieDB.movies, movieList);
+    makeChanges();
+    deleteAdv(adv);
+    sortArr(movieDB.movies);
+});
 
 
-addingFilm();
-editGenre();
-editPoster();
-deleteAdv();
     
